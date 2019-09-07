22ba30fa845494122dd1a3f938a2aa95208992d8
Pranshu Shrivastava <pranshu.shrivastava@intellectdesign.com>
2019-09-07 22:13:14 +0530

    modifications

--- src/com/programs/challenges/Tester.java
+++ src/com/programs/challenges/Tester.java
@@ -3,23 +3,29 @@ package com.programs.challenges;
 public class Tester {
 
 	public static void main(String[] args) {
-		
-		LinkedList list = new LinkedList();
-		list.add("1");
-		list.add("2");
-		list.add("8");
-		list.add("3");
-		list.add("4");
-		list.add("5");
-		list.add("6");
-		list.add("7");
-		System.out.println("Print: List: \t\t" + list);
-		System.out.println(".size(): \t\t\t\t" + list.size());
-		System.out.println(".get(3): \t\t\t\t" + list.get(3) + " (get element at index:3 - list starts from 0)");
-		System.out.println(".remove(2): \t\t\t\t" + list.remove(2) + " (element removed)");
-		System.out.println(".get(3): \t\t\t\t" + list.get(3) + " (get element at index:3 - list starts from 0)");
-		System.out.println(".size(): \t\t\t\t" + list.size());
-		System.out.println("Print again: List: \t" + list);
+		System.out.println(encode("code"));
+		System.out.println(decode(encode("code")));
+	}
+
+	static String encode(String input){
+		StringBuilder b = new StringBuilder();
+		for (int i = 0; i < input.length();i++){
+			char c = input.charAt(i);
+			b.append(c += c + i);
+		}
+		return b.reverse().toString();
+	}
+
+	static String decode(String input){
+		StringBuilder b = new StringBuilder(input);
+		input = b.reverse().toString();
+		String result = "";
+		for (int i = 0 ; i < input.length() ;i++){
+			char c = input.charAt(i);
+
+			result += Character.toString(c -= c - i);
+		}
+		return result;
 	}
 
 }
